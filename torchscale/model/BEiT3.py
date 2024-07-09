89acc05b7cad2caf9debd8bbd3d64906ab92850f
will_cjr <95006536+William-Chittavong@users.noreply.github.com>
2024-07-09 17:02:56 -0400

    put the beit3 retrieval model in the BEIT3.py

--- torchscale/model/BEiT3.py
+++ torchscale/model/BEiT3.py
@@ -3,7 +3,8 @@
 
 import torch
 import torch.nn as nn
-
+import torch.nn.functional as F
+import numpy as np
 from torchscale.architecture.encoder import Encoder
 from torchscale.component.embedding import (
     PositionalEmbedding,
@@ -15,6 +16,9 @@ from torchscale.component.multiway_network import MutliwayEmbedding
 from torchscale.component.hook import HookManager
 
 from typing import Optional
+from torchscale.component import beit3_utils as utils
+from torchscale.component.modeling_utils import BEiT3Wrapper, get_base_config, get_large_config
+
 
 class BEiT3(nn.Module):
     def __init__(self, args, hook: Optional[HookManager] = None, **kwargs):
@@ -101,3 +105,91 @@ class BEiT3(nn.Module):
         #self.hook.finalize()
 
         return encoder_out
+    
+    
+class BEiT3ForRetrieval(BEiT3Wrapper):
+    def __init__(
+            self, 
+            args,
+            hook: Optional[HookManager] = None,
+            **kwargs
+    ):
+        super(BEiT3ForRetrieval, self).__init__(args=args)
+        self.hook_manager = hook or HookManager()
+        embed_dim = args.encoder_embed_dim
+        self.language_head = nn.Linear(embed_dim, embed_dim, bias=False)
+        self.vision_head = nn.Linear(embed_dim, embed_dim, bias=False)
+        self.language_head.apply(self._init_weights)
+        self.vision_head.apply(self._init_weights)
+        self.criterion = utils.ClipLoss(
+            rank=utils.get_rank(), 
+            world_size=utils.get_world_size(), 
+        )
+        self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
+
+    def forward(self, image=None, text_description=None, padding_mask=None, only_infer=False, **kwargs):
+        if image is not None:
+            outputs = self.beit3(
+                textual_tokens=None, 
+                visual_tokens=image, 
+                text_padding_position=None,
+                hook = self.hook 
+            )
+            x = outputs["encoder_out"]
+            vision_cls = self.vision_head(x[:, 0, :])
+            vision_cls = F.normalize(vision_cls, dim=-1)
+        else:
+            vision_cls = None
+
+        if text_description is not None:
+            outputs = self.beit3(
+                textual_tokens=text_description, 
+                visual_tokens=None, 
+                text_padding_position=padding_mask, 
+            )
+            x = outputs["encoder_out"]
+            language_cls = self.language_head(x[:, 0, :])
+            language_cls = F.normalize(language_cls, dim=-1)
+        else:
+            language_cls = None
+        
+        if only_infer:
+            return vision_cls, language_cls
+        else:
+            loss, logits_per_image, logits_per_text = self.criterion(
+                vision_cls, language_cls, self.logit_scale.exp())
+            return loss, vision_cls, language_cls
+
+
+
+
+def create_beit3_retrieval_model(model_size='base', img_size=224, **kwargs):
+    """
+    Create a BEiT3 model for retrieval tasks.
+    
+    Args:
+    model_size (str): 'base' or 'large'
+    img_size (int): Image size (assuming square images)
+    **kwargs: Additional arguments to pass to the model
+    
+    Returns:
+    BEiT3ForRetrieval: The created model
+    """
+    if model_size not in ['base', 'large']:
+        raise ValueError("model_size must be either 'base' or 'large'")
+    
+    if model_size == 'base':
+        args = get_base_config(img_size=img_size, **kwargs)
+    else:  # large
+        args = get_large_config(img_size=img_size, **kwargs)
+    
+    hook_manager = HookManager()
+    
+    model = BEiT3ForRetrieval(args, hook=hook_manager, **kwargs)
+    
+    return model
+
+# Example usage:
+# base_model_224 = create_beit3_retrieval_model('base', 224)
+# large_model_384 = create_beit3_retrieval_model('large', 384)
+ 
