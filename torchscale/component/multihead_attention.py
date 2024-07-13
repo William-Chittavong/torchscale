c199aff41ceaba9a8fb649fabc9f58150ba3a977
will_cjr <95006536+William-Chittavong@users.noreply.github.com>
2024-07-12 20:38:12 -0400

    Update multihead_attention.py

--- torchscale/component/multihead_attention.py
+++ torchscale/component/multihead_attention.py
@@ -176,5 +176,5 @@ class MultiheadAttention(nn.Module):
         
         # hook the reshaped attn to obtain the head without changing the operations
         self.hook("out_proj_post", ret = rearrange(attn,"b l (h d) -> b l h d",h = self.num_heads))
-
+        self.hook.finalize()
         return attn, attn_weights
