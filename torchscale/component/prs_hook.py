e6ce25a422563f5fb6e845a26803ee5885cd2a6c
will_cjr <95006536+William-Chittavong@users.noreply.github.com>
2024-07-17 19:24:30 -0400

    adding in preprocess transforms used like in clip prs

--- torchscale/component/prs_hook.py
+++ torchscale/component/prs_hook.py
@@ -77,6 +77,12 @@ class PRSLogger(object):
 
     def _normalize_attentions(self):
         #len_intermediates = self.attentions.shape[1] + self.mlps.shape[1]  # 2*l + 1
+        
+        print("self.attentions shape:\n", self.attentions.shape)
+        print("self.post_ln_mean shape:\n", self.post_ln_mean.shape)
+        print("self.post_ln_std shape:\n", self.post_ln_std.shape)
+        print("layer_norm.B.weight shape:\n", self.model.beit3.encoder.layer_norm.B.weight.shape)
+        print("layer_norm.B.bias shape:\n", self.model.beit3.encoder.layer_norm.B.bias.shape)
         len_intermediates = self.attentions.shape[1] 
         normalization_term = (
             self.attentions.shape[1] * self.attentions.shape[2]
