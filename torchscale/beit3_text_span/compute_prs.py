fe74274914966821f7c912978da1e8d3a26631f4
will_cjr <95006536+William-Chittavong@users.noreply.github.com>
2024-07-24 15:30:53 -0400

    forgot to write to ffn.npy

--- torchscale/beit3_text_span/compute_prs.py
+++ torchscale/beit3_text_span/compute_prs.py
@@ -139,10 +139,10 @@ def main(args):
         os.path.join(args.output_dir, f"{args.dataset}_attn_{args.model}.npy"), "wb"
     ) as f:
         np.save(f, np.concatenate(attention_results, axis=0))
-    # with open(
-    #      os.path.join(args.output_dir, f"{args.dataset}_ffn_{args.model}.npy"), "wb"
-    #  ) as f:
-    #      np.save(f, np.concatenate(ffn_results, axis=0))
+    with open(
+         os.path.join(args.output_dir, f"{args.dataset}_ffn_{args.model}.npy"), "wb"
+     ) as f:
+         np.save(f, np.concatenate(ffn_results, axis=0))
     # with open(
     #      os.path.join(args.output_dir, f"{args.dataset}_cls_attn_{args.model}.npy"), "wb"
     #  ) as f:
