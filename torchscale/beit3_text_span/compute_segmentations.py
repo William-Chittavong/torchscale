ff8ee3a17045f18fbe2a47a4b7afb88a4f906317
will_cjr <95006536+William-Chittavong@users.noreply.github.com>
2024-09-05 22:00:39 -0400

    preprocess coco

--- torchscale/beit3_text_span/compute_segmentations.py
+++ torchscale/beit3_text_span/compute_segmentations.py
@@ -38,7 +38,7 @@ def get_args_parser():
                         help='threshold')
     parser.add_argument('--data_path', default='imagenet_seg/gtsegs_ijcv.mat', type=str,
                             help='dataset path')
-    parser.add_argument("--annotations",default="/home/william/project/coco_seg/annotations/instances_val2017.json", type = str , help = "annotation file")
+    #parser.add_argument("--annotations",default="/home/william/project/coco_seg/annotations/instances_val2017.json", type = str , help = "annotation file")
     parser.add_argument('--num_workers', default=10, type=int)
     parser.add_argument('--classifier_dir', default='./output_dir/')
     parser.add_argument('--batch_size', default=1, type=int,
@@ -186,8 +186,10 @@ def main(args):
                                 transform=preprocess, 
                                 target_transform=target_transform)
     else:
-        #add correct ending path
-        ds = COCOSegmentation(args.data_path, args.annotations,transform=preprocess , target_transform= target_transform)
+        ds = COCOSegmentation(root=args.data_path,
+                              split='val',  # or 'train' depending on what you want to evaluate
+                              transform=preprocess,
+                              target_transform=target_transform)
     
     dl = DataLoader(ds, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers, drop_last=False)
     iterator = tqdm.tqdm(dl)
--- torchscale/clip_utils/coco_segmentation.py
+++ torchscale/clip_utils/coco_segmentation.py
@@ -5,42 +5,124 @@ import torch.utils.data as data
 from PIL import Image
 import numpy as np
 
-class COCOSegmentation(data.Dataset):
-    CLASSES = 2  # COCO has 80 classes + 1 background + 10 extra unlabeled classes
 
-    def __init__(self, root, annFile, transform=None, target_transform=None):
-        self.root = root
-        self.coco = COCO(annFile)
-        self.ids = list(self.coco.imgs.keys())
+
+class COCOSegmentation(data.Dataset):
+    def __init__(self, root, split='train', transform=None, target_transform=None):
+        self.root = os.path.join(root, split)
+        self.split = split
         self.transform = transform
         self.target_transform = target_transform
+        
+        self.image_dir = os.path.join(self.root, 'images')
+        self.mask_dir = os.path.join(self.root, 'masks')
+        
+        self.images = [f for f in os.listdir(self.image_dir) if f.endswith('.jpg')]
+        
+    def __len__(self):
+        return len(self.images)
+    
     def __getitem__(self, index):
-        coco = self.coco
-        img_id = self.ids[index]
-        ann_ids = coco.getAnnIds(imgIds=img_id)
-        anns = coco.loadAnns(ann_ids)
+        img_name = self.images[index]
+        mask_name = img_name.replace('.jpg', '.png')
         
         # Load image
-        img = coco.loadImgs(img_id)[0]
-        path = img['file_name']
-        image = Image.open(os.path.join(self.root, path)).convert('RGB')
-
-        # Check if there are annotations
-        if len(anns) == 0:
-            # If no annotations, create a blank mask (adjust size if needed)
-            target = Image.new('L', (image.width, image.height))
-        else:
-            # Otherwise, create the mask from the first annotation
-            target = Image.fromarray(coco.annToMask(anns[0]))
-
+        image_path = os.path.join(self.image_dir, img_name)
+        image = Image.open(image_path).convert('RGB')
+        
+        # Load mask
+        mask_path = os.path.join(self.mask_dir, mask_name)
+        mask = Image.open(mask_path).convert('L')  # Convert to grayscale
+        
         if self.transform is not None:
             image = self.transform(image)
-
+        
         if self.target_transform is not None:
-            target = self.target_transform(target)
-            target = torch.from_numpy(np.array(target)).long()
+            mask = self.target_transform(mask)
+        else:
+            mask = torch.from_numpy(np.array(mask)).long()
+        
+        return image, mask
 
-        return image, target
+# class COCOSegmentation(Dataset):
+#     def __init__(self, root, annFile, transform=None, target_transform=None):
+#         self.root = root
+#         self.coco = COCO(annFile)
+#         self.transform = transform
+#         self.target_transform = target_transform
+        
+#         # Filter out images without annotations
+#         self.ids = []
+#         for img_id in self.coco.imgs.keys():
+#             ann_ids = self.coco.getAnnIds(imgIds=img_id)
+#             if len(ann_ids) > 0:
+#                 self.ids.append(img_id)
+
+#     def __getitem__(self, index):
+#         coco = self.coco
+#         img_id = self.ids[index]
+#         ann_ids = coco.getAnnIds(imgIds=img_id)
+#         anns = coco.loadAnns(ann_ids)
+        
+#         # Load image
+#         img = coco.loadImgs(img_id)[0]
+#         path = img['file_name']
+#         image = Image.open(os.path.join(self.root, path)).convert('RGB')
+
+#         # Create mask from all annotations
+#         mask = np.zeros((image.height, image.width), dtype=np.uint8)
+#         for ann in anns:
+#             mask = np.maximum(mask, coco.annToMask(ann))
+#         target = Image.fromarray(mask)
+
+#         if self.transform is not None:
+#             image = self.transform(image)
+
+#         if self.target_transform is not None:
+#             target = self.target_transform(target)
+#             target = torch.from_numpy(np.array(target)).long()
+
+#         return image, target
+
+#     def __len__(self):
+#         return len(self.ids)
+    
+# class COCOSegmentation(data.Dataset):
+#     CLASSES = 2  # COCO has 80 classes + 1 background + 10 extra unlabeled classes
+
+#     def __init__(self, root, annFile, transform=None, target_transform=None):
+#         self.root = root
+#         self.coco = COCO(annFile)
+#         self.ids = list(self.coco.imgs.keys())
+#         self.transform = transform
+#         self.target_transform = target_transform
+#     def __getitem__(self, index):
+#         coco = self.coco
+#         img_id = self.ids[index]
+#         ann_ids = coco.getAnnIds(imgIds=img_id)
+#         anns = coco.loadAnns(ann_ids)
+        
+#         # Load image
+#         img = coco.loadImgs(img_id)[0]
+#         path = img['file_name']
+#         image = Image.open(os.path.join(self.root, path)).convert('RGB')
+
+#         # Check if there are annotations
+#         if len(anns) == 0:
+#             # If no annotations, create a blank mask 
+#             target = Image.new('L', (image.width, image.height))
+#         else:
+#             # Otherwise, create the mask from the first annotation
+#             target = Image.fromarray(coco.annToMask(anns[0]))
+
+#         if self.transform is not None:
+#             image = self.transform(image)
+
+#         if self.target_transform is not None:
+#             target = self.target_transform(target)
+#             target = torch.from_numpy(np.array(target)).long()
+
+#         return image, target
 
     # def __getitem__(self, index):
     #     coco = self.coco
