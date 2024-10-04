199b0ff22efaf4944975b6e0fb3a8e125717ad37
will_cjr <95006536+William-Chittavong@users.noreply.github.com>
2024-10-04 16:21:29 -0400

    Update coco_segmentation.py

--- torchscale/clip_utils/coco_segmentation.py
+++ torchscale/clip_utils/coco_segmentation.py
@@ -57,9 +57,12 @@
 #         return len(self.coco.cats)
 
 import numpy as np
+import cv2
 from pycocotools.coco import COCO
+from pycocotools import mask as coco_mask
 from PIL import Image
 import torch
+import skimage
 
 class COCOSegmentation:
     def __init__(self, root, split='val2017', transform=None, target_transform=None):
@@ -69,50 +72,53 @@ class COCOSegmentation:
         self.ids = list(sorted(self.coco.imgs.keys()))
         self.transform = transform
         self.target_transform = target_transform
+    def create_mask(self, image_info, annotations):
+        # Create an empty mask as a numpy array
+        mask_np = np.zeros((image_info['height'], image_info['width']), dtype=np.uint16)
+
+        # Counter for the object number
+        object_number = 1
+
+        for ann in annotations:
+            if ann['image_id'] == image_info['id']:
+                # Extract segmentation polygon
+                for seg in ann['segmentation']:
+                    # Convert polygons to a binary mask and add it to the main mask
+                    rr, cc = skimage.draw.polygon(seg[1::2], seg[0::2], mask_np.shape)
+                    mask_np[rr, cc] = object_number
+                    object_number += 1
+
+        return mask_np
 
     def __getitem__(self, index):
         coco = self.coco
         img_id = self.ids[index]
         ann_ids = coco.getAnnIds(imgIds=img_id)
-
-        # Handle case where no annotations are present
-        if len(ann_ids) == 0:
-            # Return an empty mask (all zeros)
-            mask = np.zeros((coco.imgs[img_id]['height'], coco.imgs[img_id]['width']), dtype=np.uint8)
-        else:
-            # Load all annotations for this image
-            anns = coco.loadAnns(ann_ids)
-
-            # Create binary mask for the first annotation
-            mask = coco.annToMask(anns[0])
-
-            # Combine all the masks (ensuring binary values 0 or 1)
-            for ann in anns[1:]:
-                if 'segmentation' in ann:
-                    mask = np.maximum(mask, coco.annToMask(ann))
-
-        # Ensure mask is binary (0 or 1)
-        mask = np.clip(mask, 0, 1)
+        anns = coco.loadAnns(ann_ids)
 
         # Load image
         path = coco.loadImgs(img_id)[0]['file_name']
         img = Image.open(f'{self.root}/{path}').convert('RGB')
+        
+        # Create binary mask
+        img_info = coco.loadImgs(img_id)[0]
+        mask = self.create_mask(img_info, anns)
+        
+        
+        # Convert mask to PIL Image
+        mask = Image.fromarray(mask)
 
-        # Apply transformations
         if self.transform is not None:
             img = self.transform(img)
 
         if self.target_transform is not None:
-            mask = self.target_transform(Image.fromarray(mask))
-
-        # Convert mask to tensor
+            mask = self.target_transform(mask)
         mask = torch.from_numpy(np.array(mask)).long()
 
         return img, mask
 
     def __len__(self):
         return len(self.ids)
-
 # import os
 # import torch
 # import torch.utils.data as data
